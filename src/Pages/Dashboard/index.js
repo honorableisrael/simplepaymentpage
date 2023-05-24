893a1876132717e2e0f114a203d9eb3e78de1170
hilary433 <honorableisrael>
2023-05-24 17:51:37 +0100

    created modal component

---
+++ src/Pages/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Modal from "../../Component/Modal";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
+};
+
+export default Dashboard;
